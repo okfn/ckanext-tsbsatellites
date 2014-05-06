d71c6d0373d9c4195e76280de36dd60254a0f0ea
nigelb <nigelbabu@gmail.com>
2014-05-06 15:27:03 +0530

    somewhat working

---
+++ ckanext/tsbsatellites/theme/resources/js/search-autocomplete.js
@@ -0,0 +1,28 @@
+$.widget( "custom.catcomplete", $.ui.autocomplete, {
+  renderMenu: function( ul, items ) {
+    var that = this,
+    currentCategory = "";
+    $.each( items, function( index, item ) {
+      if ( item.category != currentCategory ) {
+        ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
+        currentCategory = item.category;
+      }
+      that._renderItemData( ul, item );
+    });
+  }
+});
+
+$(function() {
+  var data = [
+    { label: "annhhx10", category: "Products" },
+    { label: "annk K12", category: "Products" },
+    { label: "annttop C13", category: "Products" },
+    { label: "anders andersson", category: "People" },
+    { label: "andreas andersson", category: "People" },
+    { label: "andreas johnson", category: "People" }
+  ];
+  $( "#search" ).catcomplete({
+    delay: 0,
+    source: data
+  });
+});
