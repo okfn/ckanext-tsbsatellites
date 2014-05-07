ba43257fe7d592853d072cac04a8bf3bfca92aa4
nigelb <nigelbabu@gmail.com>
2014-05-07 17:35:59 +0530

    Make the widget prettier

--- ckanext/tsbsatellites/theme/resources/js/search-autocomplete.js
+++ ckanext/tsbsatellites/theme/resources/js/search-autocomplete.js
@@ -1,28 +1,27 @@
-$.widget( "custom.catcomplete", $.ui.autocomplete, {
-  _renderMenu: function( ul, items ) {
-    var that = this,
-    currentCategory = "";
-    $.each( items, function( index, item ) {
-      if ( item.category != currentCategory ) {
-        ul.append( "<li class='ui-autocomplete-category'>" + item.category + "</li>" );
-        currentCategory = item.category;
-      }
-      that._renderItemData( ul, item );
-    });
-   }
- });
 
 $(function() {
-  var data = [
-    { label: "annhhx10", category: "Products" },
-    { label: "annk K12", category: "Products" },
-    { label: "annttop C13", category: "Products" },
-    { label: "anders andersson", category: "People" },
-    { label: "andreas andersson", category: "People" },
-    { label: "andreas johnson", category: "People" }
-  ];
-  $( "#search" ).catcomplete({
+  $( "#search" ).autocomplete({
     delay: 0,
-    source: data
-  });
+    source: '/api/util/search',
+    minLength: 2,
+    focus: function( event, ui ) {
+      console.log(ui);
+      $( "#search" ).val( ui.item.label );
+      return false;
+    },
+    select: function( event, ui ) {
+      console.log(ui);
+      $( "#search" ).val( ui.item.label );
+    return false;
+    }
+  })
+  .data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+    var icon = "";
+    if ( 'History' === item.category ) {
+      icon = "<i class='icon-time'></i> ";
+    }
+    return $( "<li>" )
+      .append( "<a>" + icon + item.label + "</a>" )
+      .appendTo( ul );
+  };
 });
